205c6cc1741f5daf1592b75104c8b8b68b672c92
Cobolt-60 <93054720+Cobolt-60@users.noreply.github.com>
2021-11-29 21:06:33 +0100

    Update tabs.c

--- tabs.c
+++ tabs.c
@@ -1,1 +1,28 @@
+#include <Keyboard.h>
 
+void setup()
+{
+ 
+  Keyboard.begin();
+
+  
+  delay(500);
+  }
+
+
+void typeKey(int key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+
+void loop() {
+  delay(10);
+
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press('n');
+  Keyboard.releaseAll();
+
+}
